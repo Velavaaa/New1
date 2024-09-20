6db92a3ba5dd2f4ad9548d6713900ef18d635558
Velavaaa <murugaqwe63@gmail.com>
2024-09-20 20:21:17 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,6 @@
 # hello.py
-__version__ = "0.2.0"
-
-def greet(name):
-    return f"Hello, {name}!"
-
-if __name__ == "__main__":
-    print(greet("World"))
+__version__ = "0.1.0"
+print("Hello, World!")
 
 
 
